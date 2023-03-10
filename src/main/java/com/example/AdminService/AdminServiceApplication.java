a876507f8e58aed087956a701d89f5caa64ef3ee
petar <petar359>
2023-03-10 19:11:09 +0100

    project structure

--- src/main/java/com/example/AdminService/AdminServiceApplication.java
+++ src/main/java/com/example/AdminService/AdminServiceApplication.java
@@ -7,16 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping(path="/demo")
 public class AdminServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServiceApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World!";
 	}
 }
