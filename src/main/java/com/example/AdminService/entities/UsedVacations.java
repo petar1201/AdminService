ff2293f07021746be484389feebc10380a95e83a
petar <petar359>
2023-03-11 03:34:27 +0100

    entity classes added and database conected

---
+++ src/main/java/com/example/AdminService/entities/UsedVacations.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.AdminService.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+
+/**
+ *
+ * @author petar
+ */
+@Entity
+@Table(name = "usedVacations")
+@NamedQueries({
+        @NamedQuery(name = "UsedVacations.findAll", query = "SELECT u FROM UsedVacations u"),
+        @NamedQuery(name = "UsedVacations.findByEmail", query = "SELECT u FROM UsedVacations u WHERE u.email = :email"),
+        @NamedQuery(name = "UsedVacations.findByStartDate", query = "SELECT u FROM UsedVacations u WHERE u.startDate = :startDate"),
+        @NamedQuery(name = "UsedVacations.findByEndDate", query = "SELECT u FROM UsedVacations u WHERE u.endDate = :endDate")})
+public class UsedVacations implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @Nonnull
+    //@Size(min = 1, max = 40)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Nonnull
+    @Column(name = "startDate")
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    @Basic(optional = false)
+    @Nonnull
+    @Column(name = "endDate")
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+    @JoinColumn(name = "email", referencedColumnName = "email", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Employee employee;
+
+    public UsedVacations() {
+    }
+
+    public UsedVacations(String email) {
+        this.email = email;
+    }
+
+    public UsedVacations(String email, Date startDate, Date endDate) {
+        this.email = email;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UsedVacations)) {
+            return false;
+        }
+        UsedVacations other = (UsedVacations) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.UsedVacations[ email=" + email + " ]";
+    }
+
+}
